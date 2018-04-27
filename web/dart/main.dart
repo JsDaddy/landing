ffacd7c74a42d3cd7eacac6b8c71bac3a31b9afd
sfs-inep <igornepipenko@gmail.com>
2018-04-27 16:50:46 +0300

    add animated scroll

--- web/dart/main.dart
+++ web/dart/main.dart
@@ -1,4 +1,11 @@
+import 'dart:html';
+import 'scroll.dart';
 
 main() {
   print('First script');
+  querySelectorAll('#menu li > a')
+      .onClick
+      .map((MouseEvent event) =>
+          (event.target as AnchorElement).getAttribute('href'))
+      .listen((String selector) => startScroll(selector));
 }
