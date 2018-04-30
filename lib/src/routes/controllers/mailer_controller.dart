08355094d1c18a932a1e2057979d70d5b1543258
LimarenkoDenis <limarenkodenis@gmail.com>
2018-04-30 18:02:37 +0300

    form

--- lib/src/routes/controllers/mailer_controller.dart
+++ lib/src/routes/controllers/mailer_controller.dart
@@ -11,7 +11,7 @@ class MailerController extends Controller {
     try {
       Map<String,String> body = await req.lazyBody();
       var envelope = new Envelope()
-        ..from = 'icosupport@tokenize.exchange'
+        ..from = 'limarenkodenis@gmail.com'
         ..recipients.add(body['email'])
         ..subject = 'Testing the Dart Mailer library 語'
         ..text = 'This is a cool email message. Whats up? 語'
