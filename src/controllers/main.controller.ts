85e11228116fde70819e4968e38fb9977c75369d
sfs-inep <igornepipenko@gmail.com>
2018-05-07 18:02:29 +0300

    some play with nodejs

--- src/controllers/main.controller.ts
+++ src/controllers/main.controller.ts
@@ -11,13 +11,11 @@ export const mainCtrl: (app: Application) => void = (app: Application) => {
     async (_req: Request, res: Response) => {
       try {
         const mainContent: any = await new MainContentModel().getContent();
-        return res.render('main', {...mainContent, ..._req.params});
+        return res.render('main', { ...mainContent, ..._req.params });
       } catch (err) {
-        return res.status(400).json({ data: { message: err.toString() } });
+        return res.status(400)
+          .json({ data: { message: err.toString() } });
       }
     },
   );
 };
-
-
-
