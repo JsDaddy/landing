8bec92feeeaa91895c009bcad9006096f85305ff
Shemet Vitaliy <bets2305@gmail.com>
2018-05-07 15:28:57 +0300

    create footer

--- src/main.ts
+++ src/main.ts
@@ -6,21 +6,26 @@ app.use(express.static('public'));
 app.set('view engine', 'pug');
 app.get('/', (_req, res: express.Response) => {
   return res.render('main', {
-      title: '﻿Angular, React, TypeScript, JavaScript, NodeJS, VueJS development company - JSDADDY',
-      meta: [
-        {
-          name: 'description',
-          content: "JavaScript development company - JSDADDY"
-        },
-        {
-          name: 'keywords',
-          content: 'Development, JavaScript, TypeScript, Angular, MongoDB, NodeJS, VueJS, MEAN, Dart, Flutter, React Native, React, CSS, SASS, HTML, WEB'
-        }
-      ]
-    })
+    title: '﻿Angular, React, TypeScript, JavaScript, NodeJS, VueJS development company - JSDADDY',
+    meta: [
+      {
+        name: 'description',
+        content: "JavaScript development company - JSDADDY"
+      },
+      {
+        name: 'keywords',
+        content: 'Development, JavaScript, TypeScript, Angular, MongoDB, NodeJS, VueJS, MEAN, Dart, Flutter, React Native, React, CSS, SASS, HTML, WEB'
+      }
+    ],
+    footer: [
+      {
+        copyright: 'Copywriting Ⓒ 2018 JSdaddy'
+      }
+    ]
+})
 });
 app.get('*', (_req, res: express.Response) => {
-  return res.render('error',{title:'MAIN'});
+  return res.render('error', {title: 'MAIN'});
 });
 
 app.listen(3000);
