182ed26f0e1874c3e009ced7b51ec8d25c3c7627
LimarenkoDenis <limarenkodenis@gmail.com>
2018-05-10 18:33:23 +0300

    get users for courses

--- src/controllers/courses.controller.ts
+++ src/controllers/courses.controller.ts
@@ -1,10 +1,18 @@
 import * as express from 'express';
+import { UserModel } from './../models/user.model';
 
 export function coursesCtrl(app: express.Application) {
 
   app.get(
     '/:lang/courses',
-    async (_req: express.Request, res: express.Response) => res.json({data: 'Success'}),
+    async (_req: express.Request, res: express.Response) => {
+      try {
+        const users: any[] = await new UserModel().getUsers('mentor');
+        return res.render('content/courses', {...users});
+      } catch (err) {
+        return res.render('content/error');
+      }
+    },
   );
 
   app.get(
