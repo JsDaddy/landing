819e1d9eecd63fea7de8ae5ffb0455d76e622c06
sfs-inep <igornepipenko@gmail.com>
2018-05-10 16:37:09 +0300

    fix dot

--- src/controllers/courses.controller.ts
+++ src/controllers/courses.controller.ts
@@ -1,5 +1,5 @@
 import * as express from 'express';
-import {StaticContentModel} from "../models/static_content.model";
+import { StaticContentModel } from '../models/static_content.model';
 
 export function coursesCtrl(app: express.Application) {
   app.get(
@@ -7,8 +7,7 @@ export function coursesCtrl(app: express.Application) {
     async (req: express.Request, res: express.Response) => {
       try {
         const coursesContent: HashMap =
-          await new StaticContentModel().getContentHashMap(['﻿coursesBanner'], req.params.lang);
-        console.log(coursesContent);
+          await new StaticContentModel().getContentHashMap(['coursesBanner'], req.params.lang);
         return res.render('content/courses', coursesContent);
       } catch (err) {
         return res.render('content/error');
@@ -18,6 +17,6 @@ export function coursesCtrl(app: express.Application) {
 
   app.get(
     '/:lang/courses/:id',
-    async (_req: express.Request, res: express.Response) => res.json({data: 'Success'}),
+    async (_req: express.Request, res: express.Response) => res.json({ data: 'Success' }),
   );
 }
