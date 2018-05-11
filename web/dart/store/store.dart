99e7e1a45ff125d7fd45c180e7755395e911dbd8
sfs-inep <igornepipenko@gmail.com>
2018-05-11 16:11:01 +0300

    remove dart file

--- web/dart/store/store.dart
+++
@@ -1,9 +0,0 @@
-import 'package:redux/redux.dart';
-import 'package:redux_epics/redux_epics.dart';
-import 'reducer/notify.dart';
-import 'epics/notify.dart';
-
-var epicMiddleware = new EpicMiddleware(notifyEpic);
-final store = new Store<Map<String, dynamic>>(notifyReducer,
-    initialState: {'isOpen': false, 'message': ''},
-    middleware: [epicMiddleware]);
