4607ab8083e888b0da052b6f2c10ca5c85c28a41
sfs-inep <igornepipenko@gmail.com>
2018-05-16 13:39:57 +0300

    fix broken image

--- src/server/models/user.model.ts
+++ src/server/models/user.model.ts
@@ -3,8 +3,12 @@ import * as mongoose from 'mongoose';
 export class UserModel {
   public async getUsers(role?: string): Promise<any> {
     const userModel: mongoose.Model<mongoose.Document> = mongoose.model('User');
-    const query: any = role ? { roles: { $in: [ role ] } } : {};
-    return await userModel.find(query).lean();
+    const query: any = role ? {
+      active: true,
+      roles: { $in: [role] },
+    } : {};
+    return await userModel.find(query)
+      .lean();
   }
 
   public async getUser(query: any): Promise<any> {
