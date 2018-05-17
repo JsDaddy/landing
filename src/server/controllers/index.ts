7e58477f28a7713d9ad92203ce9b4050af8ff053
Shemet Vitaliy <bets2305@gmail.com>
2018-05-17 11:51:21 +0300

    create projects page

--- src/server/controllers/index.ts
+++ src/server/controllers/index.ts
@@ -1,15 +1,15 @@
 import { Application } from 'express';
-import { projectsCtrl } from './projects.controller';
 import { courseCtrl } from './course.controller';
 import { coursesCtrl } from './courses.controller';
 import { mailerCtrl } from './mailer.controller';
 import { mainCtrl } from './main.controller';
+import { projectsCtrl } from './projects.controller';
 import { sitemapCtrl } from './sitemap.controller';
 
 export const controllers: (app: Application) => void = (app: Application) => {
-  mainCtrl(app);
   courseCtrl(app);
   coursesCtrl(app);
+  mainCtrl(app);
   mailerCtrl(app);
   projectsCtrl(app);
   sitemapCtrl(app);
