69cf9dc17ba48688bdd1abfca4747ffd3224cb66
Shemet Vitaliy <bets2305@gmail.com>
2018-05-18 14:48:09 +0300

    add commynity page

--- src/server/controllers/index.ts
+++ src/server/controllers/index.ts
@@ -1,18 +1,20 @@
-import { Application } from 'express';
-import { aboutUsCtrl } from './about-us.controller';
-import { courseCtrl } from './course.controller';
-import { coursesCtrl } from './courses.controller';
-import { mailerCtrl } from './mailer.controller';
-import { mainCtrl } from './main.controller';
-import { projectsCtrl } from './projects.controller';
-import { sitemapCtrl } from './sitemap.controller';
+import {Application} from 'express';
+import {aboutUsCtrl} from './about-us.controller';
+import {communityCtrl} from './community.controller';
+import {courseCtrl} from './course.controller';
+import {coursesCtrl} from './courses.controller';
+import {mailerCtrl} from './mailer.controller';
+import {mainCtrl} from './main.controller';
+import {projectsCtrl} from './projects.controller';
+import {sitemapCtrl} from './sitemap.controller';
 
 export const controllers: (app: Application) => void = (app: Application) => {
-  courseCtrl(app);
-  coursesCtrl(app);
-  mainCtrl(app);
-  mailerCtrl(app);
-  projectsCtrl(app);
-  aboutUsCtrl(app);
-  sitemapCtrl(app);
+    courseCtrl(app);
+    coursesCtrl(app);
+    communityCtrl(app);
+    mainCtrl(app);
+    mailerCtrl(app);
+    projectsCtrl(app);
+    aboutUsCtrl(app);
+    sitemapCtrl(app);
 };
