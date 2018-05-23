2ce122485119de00a404d269c7467ae891f9ef40
Shemet Vitaliy <bets2305@gmail.com>
2018-05-23 19:18:40 +0300

    courses new page

--- src/server/controllers/courses.controller.ts
+++ src/server/controllers/courses.controller.ts
@@ -1,69 +1,70 @@
 import * as express from 'express';
-import { addCurrencyRate } from '../middleware/currency.middleware';
-import { CourseModel } from '../models/course.model';
-import { StaticContentModel } from '../models/static_content.model';
-import { UserModel } from '../models/user.model';
-import { logger } from './../main';
-import { UtilsService } from './../services/utils.service';
+import {addCurrencyRate} from '../middleware/currency.middleware';
+import {CourseModel} from '../models/course.model';
+import {StaticContentModel} from '../models/static_content.model';
+import {UserModel} from '../models/user.model';
+import {logger} from './../main';
+import {UtilsService} from './../services/utils.service';
 
 export function coursesCtrl(app: express.Application) {
-  app.get(
-    '/:lang/courses',
-    addCurrencyRate(app),
-    async (req: express.Request, res: express.Response) => {
-      let { lang } = req.params;
-      try {
-        const coursesContent: IHashMap = await new StaticContentModel().getContentHashMap([
-          { query: 'coursesHead', replace: 'head', rewrite: true },
-          { query: 'coursesMenu', replace: 'mainMenu' },
-          'coursesThumbs',
-          'coursesBanner',
-          'about',
-          'advantagesCourses',
-          'contactsCourses',
-          'mentors',
-          'footer',
-        ], req.params.lang);
+    app.get(
+        '/:lang/courses',
+        addCurrencyRate(app),
+        async (req: express.Request, res: express.Response) => {
+            let {lang} = req.params;
+            try {
+                const coursesContent: IHashMap = await new StaticContentModel().getContentHashMap([
+                    {query: 'coursesHead', replace: 'head', rewrite: true},
+                    {query: 'coursesMenu', replace: 'mainMenu'},
+                    'coursesThumbs',
+                    'coursesBanner',
+                    'about',
+                    'advantagesCourses',
+                    'contactsCourses',
+                    'mentors',
+                    'footer',
+                    'faq',
+                ], req.params.lang);
 
-        const users: any[] = await new UserModel().getUsers('mentor');
+                const users: any[] = await new UserModel().getUsers('mentor');
 
-        coursesContent.mainMenu.content.languages =
-          new UtilsService().getLangulagesLinks(coursesContent.mainMenu.content.languages, lang);
+                coursesContent.mainMenu.content.languages =
+                    new UtilsService().getLangulagesLinks(coursesContent.mainMenu.content.languages, lang);
 
-        coursesContent.mentors.content = users.map((user) => {
-          return {
-            ...user,
-            about: user.about[lang],
-            firstName: user.firstName[lang],
-            lastName: user.lastName[lang],
-          };
-        });
-        const coursesThumbs: any[] = await new CourseModel().getAllContent({ lang });
-        coursesContent.coursesThumbs.content = coursesThumbs
-          .map((thumb) => {
-            return {
-              ...thumb,
-              href: `courses/${thumb.name}`,
-              price: `${Math.round(req.params.currency * thumb.price)}\₴ (~${thumb.price}\$)`,
-            };
-          });
-        const courses: any = coursesThumbs
-          .reduce((acc: any, next: any) => [...acc, { id: next.name, title: next.title }], []);
-        return res.render(
-          'content/courses',
-          {
-            ...coursesContent,
-            courses,
-            lang,
-            users,
-          });
-      } catch (err) {
-        logger.log('error', err);
-        // TODO aggregate from db
-        lang = app.get('config').get('langs').includes(lang) ? lang : 'en';
-        return res.render(`content/error-${lang}`);
-      }
-    },
-  );
+                coursesContent.mentors.content = users.map((user) => {
+                    return {
+                        ...user,
+                        about: user.about[lang],
+                        firstName: user.firstName[lang],
+                        lastName: user.lastName[lang],
+                    };
+                });
+                const coursesThumbs: any[] = await new CourseModel().getAllContent({lang});
+                coursesContent.coursesThumbs.content = coursesThumbs
+                    .map((thumb) => {
+                        return {
+                            ...thumb,
+                            href: `courses/${thumb.name}`,
+                            price: `${Math.round(req.params.currency * thumb.price)}\₴ (~${thumb.price}\$)`,
+                        };
+                    });
+                const courses: any = coursesThumbs
+                    .reduce((acc: any, next: any) => [...acc, {id: next.name, title: next.title}], []);
+                return res.render(
+                    'content/courses',
+                    {
+                        ...coursesContent,
+                        courses,
+                        lang,
+                        users,
+                    });
+            } catch (err) {
+                logger.log('error', err);
+                // TODO aggregate from db
+                lang = app.get('config').get('langs').includes(lang) ? lang : 'en';
+                return res.render(`content/error-${lang}`);
+            }
+        },
+    );
 
 }
