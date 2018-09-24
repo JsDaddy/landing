3447ddddff410b4b6450c28710a019990a895a77
unnamed92 <miketkachenko92@gmail.com>
2018-09-24 16:43:43 +0300

    backend page

--- src/server/controllers/index.ts
+++ src/server/controllers/index.ts
@@ -1,5 +1,6 @@
 import {Application} from 'express';
 import {aboutUsCtrl} from './about-us.controller';
+import { cloudDevelopmentCtrl } from './cloud_development.controller';
 import {mainCtrl} from './main.controller';
 import { webDevelopmentCtrl } from './web-development.controller';
 
@@ -7,4 +8,5 @@ export const controllers: (app: Application) => void = (app: Application) => {
     mainCtrl(app);
     aboutUsCtrl(app);
     webDevelopmentCtrl(app);
+    cloudDevelopmentCtrl(app);
 };
