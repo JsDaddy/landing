79275a8e5c7a5aff716f3d8c19361900a2630b6d
alexeydolia <adol@jsdaddy.io>
2019-03-19 18:16:05 +0200

    update

--- src/server/controllers/main.controller.ts
+++ src/server/controllers/main.controller.ts
@@ -1,4 +1,5 @@
 import * as express from 'express';
+import { AboutUsModel } from '../models/about-us.model';
 import { FooterModel } from '../models/footer.model';
 import {HeaderModel} from '../models/header.model';
 import {MainPageModel} from '../models/main-page.model';
@@ -16,10 +17,12 @@ export const mainCtrl = (app: express.Application) => {
           'headerMenu',
           'mainPage',
           'technologies',
+          'aboutCompany',
           'footerNew',
         ]);
         mainContent.headerMenu = await new HeaderModel().getContent({name: 'headerMenu'});
         mainContent.technologies = await new TechnologiesModel().getContent({name: 'technologies'});
+        mainContent.aboutCompany = await new AboutUsModel().getContent({name: 'aboutCompany'});
         mainContent.mainPage = await new MainPageModel().getContent({name: 'mainPage'});
         mainContent.footerNew = await new FooterModel().getContent({name: 'footerNew'});
         return res.render('content/main', mainContent);
