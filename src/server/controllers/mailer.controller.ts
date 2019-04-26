e4356377833fa761a3281e2055b49f75cad6294a
ValeriaKochegarova <lerchik199629@gmail.com>
2019-04-26 18:56:07 +0300

    parse json to string

--- src/server/controllers/mailer.controller.ts
+++ src/server/controllers/mailer.controller.ts
@@ -9,10 +9,9 @@ export const mailerCtrl = (app: express.Application) => {
     '/mail/contacts',
     async (req: express.Request, res: express.Response) => {
       try {
-        await new MailerService().sendMail(req.body, 'contacts');
+        // await new MailerService().sendMail(req.body, 'contacts');
         await new MailerService().sendMail({ copyEmail, ...req.body }, 'copy');
-
-        return res.json({ message: {ru: 'Спасибо', en: 'Email sent' }, type: 'Success' });
+        return res.send(`<div> Name: ${req.body.name} </div>`);
       } catch (err) {
         // logger.log('error', err);
         return res.json({ message: {ru: 'Что-то пошло не так', en: 'Something went wrong' }, type: 'Error' });
--- src/web/scripts/common.ts
+++ src/web/scripts/common.ts
@@ -109,6 +109,8 @@ $(document).ready(() => {
   });
   $('.popup-form__input-wrapper_invalid-message').hide();
   $('#form__submit').click(() => {
+    // tslint:disable-next-line:no-console
+    console.log('444444');
     const emailRegExp = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
     const name = $('input#name').val();
     if (name === '') {
@@ -143,6 +145,7 @@ $(document).ready(() => {
       $('#textarea_error').hide();
       $('textarea#message').removeClass('popup-form__input-wrapper_invalid');
     }
+        // tslint:disable-next-line:align
     if (name !== '' && email !== '' && message !== '') {
       $('.popup-form').submit(() => {
         $('#form__submit').prop('disabled', true);
@@ -165,7 +168,7 @@ $(document).ready(() => {
             }, 3000);
           },
           type: 'POST',
-          url: 'http://jsdaddy.io/mail/contacts',
+          url: 'http://localhost:3000/mail/contacts',
         });
         return false;
       });
@@ -173,6 +176,42 @@ $(document).ready(() => {
     }
     return;
   });
+
+  // function populateTemplate(data: any) {
+  //   // tslint:disable-next-line:no-console
+  //   // console.log('AAAAAA');
+  //   const keys = Object.keys(data);
+  //    // tslint:disable-next-line:prefer-for-of
+  //    for (let i = 0; i < keys.length; i ++) {
+  //     const key = keys[i];
+  //     // tslint:disable-next-line:no-console
+  //     const target = 'json-' + key;
+  //     // tslint:disable
+  //     console.log('' , keys[i], target);
+  //     console.log( 'RRRRRR', document.getElementById('json-' + keys[i]) as any);
+  //     console.log('data[key]', data[key]);
+  //     // tslint:disable-next-line:align
+  //     (document.getElementById('json-' + keys[i]) as any).innerHTML = data[key];
+  //      }
+  //   }
+  // document.getElementById('form__submit')!
+  //         .addEventListener('click', (e) => {
+  //          e.preventDefault();
+  //          // tslint:disable-next-line:no-console
+  //          console.log('BBBBBB');
+  //          const name = (document.getElementById('name') as any).value;
+  //          const email = (document.getElementById('email') as any).value;
+  //          const message = (document.getElementById('message') as any).value;
+  //          // tslint:disable-next-line
+  //          console.log(email, name, message);
+  //          const data = {
+  //           email,
+  //           message,
+  //           name,
+  //          };
+  //          // console.log('vvv', v);
+  //          populateTemplate(data);
+  //        });
 
   $('.testimonials-wrapper').on('setPosition', () => {
     $('.testimonials-wrapper')
