542e3209beb4bc2a9ab34220ce3442f1fdeac245
Developer <developer@Developers-iMac-2.local>
2020-10-29 16:02:17 +0200

    feature: creating static content [ref: no-ref]

--- src/server/controllers/about-us.controller.ts
+++ src/server/controllers/about-us.controller.ts
@@ -1,27 +1,32 @@
-import * as express from 'express';
-import {AboutUsModel} from '../models/about-us.model';
-import { FooterModel } from '../models/footer.model';
-import {HeaderModel} from '../models/header.model';
-import {StaticContentModel} from '../models/static_content.model';
+import * as express from "express";
+import { AboutUsModel } from "../models/about-us.model";
+import { FooterModel } from "../models/footer.model";
+import { HeaderModel } from "../models/header.model";
+import { StaticContentModel } from "../models/static_content.model";
 
 export const aboutUsCtrl = (app: express.Application) => {
-    app.get(
-        '/about-us',
-        async (_req: express.Request, res: express.Response) => {
-            try {
-                const aboutUsContent: IHashMap = await new StaticContentModel().getContentHashMap([
-                    {query: 'mainHead', replace: 'head', rewrite: true},
-                    'aboutCompany',
-                    'headerMenu',
-                    'footerNew',
-                ]);
-                aboutUsContent.aboutCompany = await new AboutUsModel().getContent({name: 'aboutCompany'});
-                aboutUsContent.headerMenu = await new HeaderModel().getContent({name: 'headerMenu'});
-                aboutUsContent.footerNew = await new FooterModel().getContent({name: 'footerNew'});
-                return res.render('content/about-us', aboutUsContent);
-            } catch (err) {
-                return res.render('content/error-en');
-            }
-        },
-    );
+  app.get("/about-us", async (_req: express.Request, res: express.Response) => {
+    try {
+      const aboutUsContent: IHashMap = await new StaticContentModel().getContentHashMap(
+        [
+          { query: "mainHead", replace: "head", rewrite: true },
+          "aboutCompany",
+          "headerMenu",
+          "footerNew",
+        ]
+      );
+      aboutUsContent.aboutCompany = await new AboutUsModel().getContent({
+        name: "aboutCompany",
+      });
+      aboutUsContent.headerMenu = await new HeaderModel().getContent({
+        name: "headerMenu",
+      });
+      aboutUsContent.footerNew = await new FooterModel().getContent({
+        name: "footerNew",
+      });
+      return res.render("content/about-us", aboutUsContent);
+    } catch (err) {
+      return res.render("content/error-en");
+    }
+  });
 };
--- src/server/main.ts
+++ src/server/main.ts
@@ -1,26 +1,28 @@
-import * as compression from 'compression';
-import * as config from 'config';
-import * as express from 'express';
+import * as compression from "compression";
+import * as config from "config";
+import * as express from "express";
 
 const app = express();
-import './schema/index';
+import "./schema/index";
 
-import { appConf } from './config/app.config';
-import { connectToDb } from './config/db.config';
-import { controllers } from './controllers';
+import { appConf } from "./config/app.config";
+import { connectToDb } from "./config/db.config";
+import { controllers } from "./controllers";
 
 connectToDb();
 appConf(app);
 controllers(app);
 
-app.set('config', config);
+app.set("config", config);
 app.use(compression());
-app.use(express.static('public'));
-app.set('view engine', 'pug');
+app.use(express.static("public"));
+app.set("view engine", "pug");
 
-app.get('*', (req, res: express.Response) => {
-  const urlLang = req.url.split('/')[1];
-  const lang = app.get('config').get('langs').includes(urlLang) ? urlLang : 'en';
+app.get("*", (req, res: express.Response) => {
+  const urlLang = req.url.split("/")[1];
+  const lang = app.get("config").get("langs").includes(urlLang)
+    ? urlLang
+    : "en";
   return res.render(`content/error-${lang}`);
 });
 
